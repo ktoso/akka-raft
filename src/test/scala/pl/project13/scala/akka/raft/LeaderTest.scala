98e9d9531b64ec2aac7d3f7ffdb83b12346bff2d
Konrad Malawski <konrad.malawski@project13.pl>
2014-02-02 20:36:18 +0000

    changed test to check if leader can respond with cluster's config

--- src/test/scala/pl/project13/scala/akka/raft/LeaderTest.scala
+++ src/test/scala/pl/project13/scala/akka/raft/LeaderTest.scala
@@ -1,31 +1,30 @@
 package pl.project13.scala.akka.raft
 
 import pl.project13.scala.akka.raft.protocol._
-import akka.testkit.{TestKit, TestProbe, TestFSMRef}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe, TestFSMRef}
 import org.scalatest._
 import akka.actor.ActorSystem
 import pl.project13.scala.akka.raft.example.WordConcatRaftActor
 import pl.project13.scala.akka.raft.model.{Term, LogIndexMap}
-import pl.project13.scala.akka.raft.protocol.RaftStates.Leader
 import pl.project13.scala.akka.raft.example.protocol._
 
-class LeaderTest extends TestKit(ActorSystem("test-system")) with FlatSpecLike
-  with Matchers
+class LeaderTest extends TestKit(ActorSystem("test-system")) with FlatSpecLike with Matchers
+  with ImplicitSender
   with BeforeAndAfterEach with BeforeAndAfterAll {
 
   behavior of "Leader"
 
   val leader = TestFSMRef(new WordConcatRaftActor)
 
-  var data: Meta = _
+  var data: LeaderMeta = _
   
   override def beforeEach() {
     super.beforeEach()
     data = Meta.initial(leader)
       .copy(
         currentTerm = Term(1),
-        members = Set(leader)
-      )
+        config = RaftConfiguration(leader)
+      ).forNewElection.forLeader
   }
 
   it should "commit an entry once it has been written by the majority of the Followers" in {
@@ -44,11 +43,25 @@ class LeaderTest extends TestKit(ActorSystem("test-system")) with FlatSpecLike
     replicatedLog += model.Entry(AppendWord("c"), Term(1), 3)
 
     // when
-    val committedLog = actor.maybeCommitEntry(matchIndex, replicatedLog)
+    val (committedLog, data2) = actor.maybeCommitEntry(data, matchIndex, replicatedLog)
+    data = data2 // todo super ugly, fixme
 
     // then
     actor.replicatedLog.committedIndex should equal (-1)
     committedLog.committedIndex should equal (2)
+  }
+
+  it should "reply with it's current configuration when asked to" in {
+    // note: this is used when an actor has died and starts again in Init state
+
+    // given
+    leader.setState(Leader, data)
+
+    // when
+    leader ! RequestConfiguration
+
+    // then
+    expectMsg(ChangeConfiguration(StableRaftConfiguration(Set(leader))))
   }
 
 }
