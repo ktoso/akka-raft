45d6887487efee100b8a6500654d35c4d0cd9b40
Konrad Malawski <konrad.malawski@project13.pl>
2014-02-08 22:39:17 +0000

    more docs

--- src/main/scala/pl/project13/scala/akka/raft/ReplicatedStateMachine.scala
+++ src/main/scala/pl/project13/scala/akka/raft/ReplicatedStateMachine.scala
@@ -1,5 +1,17 @@
 package pl.project13.scala.akka.raft
 
+import pl.project13.scala.akka.raft.model.{RaftSnapshotMetadata, RaftSnapshot}
+import scala.concurrent.Future
+
+/**
+ * The main API trait for RaftActors.
+ * By implementing these methods you're implementing the replicated state machine.
+ *
+ * Messages sent to `apply` are guaranteed by Raft to have been '''committed''', so they're safe to apply to your STM.
+ *
+ * In order to start using replicated log snapshotting, you just need to override the `prepareSnapshot` method,
+ * which will be called each time Raft decides to take a snapshot of the replicated journal (how often that happens is configurable).
+ */
 trait ReplicatedStateMachine {
 
   type ReplicatedStateMachineApply = PartialFunction[Any, Any]
@@ -18,7 +30,40 @@ trait ReplicatedStateMachine {
    *
    * You can treat this as a raft equivalent of receive, with the difference that apply is guaranteed to be called,
    * only after the message has been propagated to the majority of members.
+   *
+   * '''Log compaction and snapshots''':
+   * Match for [[pl.project13.scala.akka.raft.protocol.RaftProtocol.InstallSnapshot]] in order to install snapshots
+   * to your internal state machine if you're using log compactation (see `prepareSnapshot`)
+   *
+   * @return the returned value will be sent back to the client issuing the command.
+   *         The reply is only sent once, by the current raft leader.
    */
   def apply: ReplicatedStateMachineApply
-  
+
+
+  /**
+   * Called whenever compaction is performed on the raft replicated log.
+   *
+   * Log compaction is required in order to maintain long running raft clusters
+   *
+   * The produced snapshot MUST include [[pl.project13.scala.akka.raft.model.RaftSnapshotMetadata]]
+   * obtained as the parameter in this call. The simplest snapshotting example would be to complete with the current state:
+   *
+   * {{{
+   *   class SummingActor extends RaftActor {
+   *     var sum: Int = 0
+   *
+   *     def receive = { case i: Int => sum += i }
+   *
+   *     // compaction is simple, we can just store the current state
+   *     def onCompaction(meta: RaftSnapshotMetadata) =
+   *       Future(Some(RaftSnapshot(meta, sum)))
+   *   }
+   * }}}
+   *
+   * @return if you don't want to store a snapshot (default impl), you can just complete the Future with None,
+   *         otherwise, return the snapshot data you want to store
+   */
+  def prepareSnapshot(snapshotMetadata: RaftSnapshotMetadata): Future[Option[RaftSnapshot]] =
+    Future.successful(None)
 }
