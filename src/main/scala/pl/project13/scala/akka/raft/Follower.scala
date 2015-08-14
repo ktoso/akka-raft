1b0799de13a1be899f438a9a3e8ae370c4b8b6a7
colin-scott <cs@cs.berkeley.edu>
2015-08-13 21:25:08 -0700

    Reject vote if leader's log isn't up-to-date

--- src/main/scala/pl/project13/scala/akka/raft/Follower.scala
+++ src/main/scala/pl/project13/scala/akka/raft/Follower.scala
@@ -18,31 +18,60 @@ private[raft] trait Follower {
       sender() ! LeaderIs(recentlyContactedByLeader, Some(msg))
       stay()
 
-    // start of election
+    // election
+    case Event(r @ RequestVote(term, candidate, lastLogTerm, lastLogIndex), m: Meta)
+      if term > m.currentTerm =>
+      log.info("Received newer {}. Current term is {}.", term, m.currentTerm)
+      // Let the next case clause deal with it, after we've updated our Term
+      m.clusterSelf forward r
+      stay() using m.withTerm(term)
 
-    case Event(msg: RequestVote, m: Meta) if msg.term < m.currentTerm =>
-      log.info("Rejecting RequestVote msg by {} in {}. Received stale {}.", candidate, m.currentTerm, msg.term)
-      candidate ! DeclineCandidate(m.currentTerm)
-      stay()
+    case Event(RequestVote(term, candidate, lastLogTerm, lastLogIndex), m: Meta)
+      if m.canVoteIn(term, candidate) =>
 
-    case Event(msg: RequestVote, m: Meta) if msg.term >= m.currentTerm =>
-      val msgTerm = msg.term
-      if (m.canVoteIn(msgTerm)) {
-        log.info("Voting for {} in {}", candidate, msgTerm)
-        candidate ! VoteCandidate(msgTerm)
-        stay() using m.withVote(msgTerm, candidate)
+      resetElectionDeadline()
+      // Check if the log is up-to-date before granting vote.
+      // Raft determines which of two logs is more up-to-date
+      // by comparing the index and term of the last entries in the
+      // logs. If the logs have last entries with different terms, then
+      // the log with the later term is more up-to-date. If the logs
+      // end with the same term, then whichever log is longer is
+      // more up-to-date.
+      if (lastLogTerm < replicatedLog.lastTerm) {
+        log.info("Rejecting vote for {}, and {}. Candidate's lastLogTerm: {} < ours: {}",
+          candidate, term, lastLogTerm, replicatedLog.lastTerm)
+        sender ! DeclineCandidate(m.currentTerm)
+        stay()
+      } else if (lastLogTerm == replicatedLog.lastTerm &&
+                 lastLogIndex < replicatedLog.lastIndex) {
+        log.info("Rejecting vote for {}, and {}. Candidate's lastLogIndex: {} < ours: {}",
+          candidate, term, lastLogIndex, replicatedLog.lastIndex)
+        sender ! DeclineCandidate(m.currentTerm)
+        stay()
       } else {
-        log.info("Rejecting RequestVote msg by {} in {}. Already voted for {}", candidate, msgTerm, m.currentTerm, m.votes.get(msgTerm))
-        sender ! DeclineCandidate(msgTerm)
-        stay() using m.withTerm(msgTerm)
+        log.info("Voting for {} in {}", candidate, term)
+        sender ! VoteCandidate(m.currentTerm)
+
+        stay() using m.withVote(term, candidate)
       }
+
+    case Event(RequestVote(term, candidateId, lastLogTerm, lastLogIndex), m: Meta) =>
+      log.info("Rejecting vote for {}, and {}, currentTerm: {}, already voted for: {}", candidate(), term, m.currentTerm, m.votes.get(term))
+      sender ! DeclineCandidate(m.currentTerm)
+      stay()
 
     // end of election
 
     // take writes
     case Event(msg: AppendEntries[Command], m: Meta) =>
-      senderIsCurrentLeader()
-      appendEntries(msg, m)
+      // First check the consistency of this request
+      if (!replicatedLog.containsMatchingEntry(msg.prevLogTerm, msg.prevLogIndex)) {
+        log.info("Rejecting write (inconsistent log): {} {} {} ", msg.prevLogIndex, msg.prevLogTerm, replicatedLog)
+        leader ! AppendRejected(m.currentTerm)
+        stay()
+      } else {
+        appendEntries(msg, m)
+      }
 
     // end of take writes
 
@@ -61,24 +90,20 @@ private[raft] trait Follower {
     implicit val self = m.clusterSelf // todo this is getting pretty crap, revert to having Cluster awareness a trait IMO
 
     if (leaderIsLagging(msg, m)) {
-      if (msg.isNotHeartbeat) {
-        log.info("Rejecting write (Leader is lagging) of: " + msg + "; " + replicatedLog)
-        leader ! AppendRejected(m.currentTerm, replicatedLog.lastIndex) // no need to respond if only heartbeat
-      }
-      stay()
-
-    } else if (msg.isHeartbeat) {
-      acceptHeartbeat()
-
-    } else {
-      log.debug("Appending: " + msg.entries)
-      leader ! append(msg.entries, m)
-      replicatedLog = commitUntilLeadersIndex(m, msg)
-      
-      val meta = maybeUpdateConfiguration(m, msg.entries.map(_.command))
-      val metaWithUpdatedTerm = meta.copy(currentTerm = replicatedLog.lastTerm)
-      acceptHeartbeat() using metaWithUpdatedTerm
+      log.info("Rejecting write (Leader is lagging) of: " + msg + "; " + replicatedLog)
+      leader ! AppendRejected(m.currentTerm)
+      return stay()
     }
+
+    senderIsCurrentLeader()
+
+    log.debug("Appending: " + msg.entries)
+    leader ! append(msg.entries, m)
+    replicatedLog = commitUntilLeadersIndex(m, msg)
+
+    val meta = maybeUpdateConfiguration(m, msg.entries.map(_.command))
+    val metaWithUpdatedTerm = meta.copy(currentTerm = replicatedLog.lastTerm)
+    acceptHeartbeat() using metaWithUpdatedTerm
   }
 
   def leaderIsLagging(msg: AppendEntries[Command], m: Meta): Boolean =
@@ -134,5 +159,4 @@ private[raft] trait Follower {
       // simply ignore applying cluster configurations onto the client state machine,
       // it's an internal thing and the client does not care about cluster config change.
   }
-  
 }
